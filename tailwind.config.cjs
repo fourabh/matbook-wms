ce8b3d1c5678ac6a1f5ff35ec1897e8673a9da97
sourabh <raisourabh85@gmail.com>
2025-03-29 14:38:24 +0530

    initial-setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+} 
